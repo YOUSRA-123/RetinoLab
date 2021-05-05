0127c5f2fdc0367d56e1e503c2720a00bfdb25bf
YOUSRA-123 <cherifyousra123@gmail.com>
2021-05-05 16:41:55 +0000

    test

--- app/src/main/java/com/example/retinolab/DemandeConsultation.java
+++ app/src/main/java/com/example/retinolab/DemandeConsultation.java
@@ -1,4 +1,14 @@
 package com.example.retinolab;
 
-public class DeamndeConsultation {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_demandeConsultation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demande_consultation);
+    }
 }
